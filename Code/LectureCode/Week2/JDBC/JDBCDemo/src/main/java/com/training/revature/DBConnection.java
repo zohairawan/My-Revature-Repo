51765291dda980e02ef7fdcbeee8118f1103f0ef
zohairawan <zawan@luc.com>
2022-05-20 22:46:39 -0700

    Added property object to take key/value pairs from reader object

--- Code/LectureCode/Week2/JDBC/JDBCDemo/src/main/java/com/training/revature/DBConnection.java
+++ Code/LectureCode/Week2/JDBC/JDBCDemo/src/main/java/com/training/revature/DBConnection.java
@@ -1,8 +1,12 @@
 package com.training.revature;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
@@ -11,14 +15,25 @@ public class DBConnection {
         //Local variable
         Connection conn = null;
         try {
-            //1.Load the Driver for our specific database
-            Class.forName("org.postgresql.Driver");
+            //1.Retrieve the file containing the Driver and connection information and store it in reader
+            FileReader reader = new FileReader("C:\\Users\\Zohair\\IdeaProjects\\Revature\\Code\\LectureCode\\Week2\\DBConnection.properties");
 
-            //2.Create a connection to the database
-            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "root");
+            //2.Properties object will be used to store the key/value pairs of the reader file(DBConnection.properties)
+            Properties properties = new Properties();
+            properties.load(reader);
+
+            //3.Load the Driver for our specific database by using the property object instead of hardcoding
+            Class.forName(properties.getProperty("driver"));
+
+            //4.Create a connection to the database by using properties object to get key(url,user,password)
+            conn = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("username"), properties.getProperty("password"));
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
