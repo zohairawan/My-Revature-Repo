663acc7b5369ccc6be6fae3c1f64a992ff288211
zohairawan <zawan@luc.com>
2022-05-21 18:38:01 -0700

    Design Pattern Folder

--- Code/LectureCode/Week3/DesignPattern/src/main/java/singleton/SingletonClientDemo.java
+++ Code/LectureCode/Week3/DesignPattern/src/main/java/singleton/SingletonClientDemo.java
@@ -1,2 +1,17 @@
-package singleton;public class SingletonClientDemo {
+
+/**
+ * Program that demonstrates the use of Singleton Design Pattern
+ * Singleton Design Pattern creates one object to be used for all users of the app
+ * This shows that only one object gets created
+ */
+package singleton;
+
+public class SingletonClientDemo {
+    public static void main(String[] args) {
+        //Even though it looks like we are creating 3 SingletonDemo objects it's actually just one
+        SingletonDemo obj1 = SingletonDemo.getInstance();
+        SingletonDemo obj2 = SingletonDemo.getInstance();
+        SingletonDemo obj3 = SingletonDemo.getInstance();
+
+    }
 }
--- Code/LectureCode/Week3/DesignPattern/src/main/java/singleton/SingletonDemo.java
+++ Code/LectureCode/Week3/DesignPattern/src/main/java/singleton/SingletonDemo.java
@@ -1,2 +1,25 @@
-package singleton;public class SingletonDemo {
+/**
+ * Program that demonstrates the use of Singleton Design Pattern
+ * Singleton Design Pattern creates one object to be used for all users of the app
+ */
+
+package singleton;
+
+public class SingletonDemo {
+
+    //Declaring object of this class as static, so it's a class object
+    static SingletonDemo obj;
+
+    //1.Create a private constructor so no other classes can create an object of this class
+    private SingletonDemo() {
+        System.out.println("Object created");
+    }
+
+    //2.Create a method that returns an object of this class
+    public static SingletonDemo getInstance() {
+        if (obj == null) {
+            obj = new SingletonDemo();
+        }
+        return obj;
+    }
 }
