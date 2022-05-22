cc64df60c4c7a3b309ddc031d29ff230ab730508
zohairawan <zawan@luc.com>
2022-05-21 17:23:27 -0700

    Set up @RestController and @RequestMapping

--- Code/Projects/project1/src/main/java/com/project1/training/revature/Project1Application.java
+++ Code/Projects/project1/src/main/java/com/project1/training/revature/Project1Application.java
@@ -2,12 +2,21 @@ package com.project1.training.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Project1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project1Application.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(Project1Application.class, args);}
 
+	@RequestMapping()			//localhost:8089
+	public String index() {return "Welcome to index page";}
+
+	@RequestMapping("/registerUser")			//localhost:8089/registerUser
+	public String registerUser() {return "Welcome to register user page";}
+
+	@RequestMapping("/login")			//localhost:8089/login
+	public String login() {return "Welcome to login page";}
 }
