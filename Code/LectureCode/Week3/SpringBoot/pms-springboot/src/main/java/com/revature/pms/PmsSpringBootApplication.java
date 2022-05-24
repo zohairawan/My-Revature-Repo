b7a61027f1326d1c929de5810e74a97dffd398fe
zohairawan <zawan@luc.com>
2022-05-24 14:41:53 -0700

    Push

--- Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/PmsSpringBootApplication.java
+++ Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/PmsSpringBootApplication.java
@@ -1,5 +1,6 @@
 package com.revature.pms;
 
+import com.revature.pms.controller.ProductController;
 import com.revature.pms.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PmsSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PmsSpringBootApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(PmsSpringBootApplication.class, args);
+
 	}
 
 }
--- Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/controller/ProductController.java
+++ Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/controller/ProductController.java
@@ -6,10 +6,7 @@
 package com.revature.pms.controller;
 
 import com.revature.pms.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 //This is at the class level so if you want enter into this controller then you have to use localhost:8084/product
@@ -38,19 +35,25 @@ public class ProductController {
         return product;
     }
 
-    //localhost:8084/product/filterProductByPrice/100/and/500
+
     //Here is the result for product in the price range of 100 and 500
-    @GetMapping("/filterProductByPrice/{lowerPrice}/and/{upperPrice}")
+    @GetMapping("/filterProductByPrice/{lowerPrice}/and/{upperPrice}") //localhost:8084/product/filterProductByPrice/100/and/500
     public String filterProductByPrice(@PathVariable("lowerPrice")int lowerPrice,@PathVariable("upperPrice")int upperPrice){
-        //call the methods to fetch product details of this productname
+        //call the methods to fetch product details of price range
         return "Here is the result for product in the price range of "+lowerPrice+" and "+ upperPrice;
     }
 
-    //localhost:8084/product/outOfStockProductDetails/350
+
     //Output: Order with qoh less than 50 should be reordered immediately
     //Output: Order with qoh less than 350 should be reordered immediately
-    @GetMapping("/outOfStockProductDetails/{productStock}")
+    @GetMapping("/outOfStockProductDetails/{productStock}") //localhost:8084/product/outOfStockProductDetails/350
     public String outOfStockProductDetails(@PathVariable("productStock") int productStock) {
         return "Order with qoh less than " + productStock + " should be reordered immediately";
+    }
+
+    //Deletes a single product based on productId
+    @DeleteMapping("{pId}") //localhost:8084/product/123
+    public String deleteProductById(@PathVariable("pId") int productId) {
+        return "Product " + productId + " successfully deleted";
     }
 }
