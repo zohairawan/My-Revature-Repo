1ad5db5a7b0a2daccc1e36dc3c947583cc10766f
zohairawan <zawan@luc.com>
2022-05-24 18:16:33 -0700

    Minor changes to notes

--- Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/PmsSpringBootApplication.java
+++ Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/PmsSpringBootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PmsSpringBootApplication {
 
 	public static void main(String[] args) {SpringApplication.run(PmsSpringBootApplication.class, args);
-		
+
 	}
 
 }
