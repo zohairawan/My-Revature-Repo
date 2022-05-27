d19316c17bf6e6f31299c77af2fc2192871c0bab
zohairawan <zawan@luc.com>
2022-05-26 22:44:01 -0700

    Incremental changes

--- Code/Projects/project1/src/main/java/com/project1/training/revature/controller/ItemController.java
+++ Code/Projects/project1/src/main/java/com/project1/training/revature/controller/ItemController.java
@@ -1,9 +1,21 @@
 /**
- *
+ * Controller class is meant to contain HTTP methods like: Get, Post, Put, Delete
+ * We will use Postman app to send HTTP methods(Get,Post,Put,Delete) and those methods will be sent here to actually perform those operations
  */
 
 package com.project1.training.revature.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("item")
 public class ItemController {
 
+    //http://localhost:8085/item/getItems
+    @GetMapping("getItems")
+    public String getAllItems() {
+        return "Displaying all your items";
+    }
 }
--- Code/Projects/project1/src/main/java/com/project1/training/revature/controller/UserController.java
+++ Code/Projects/project1/src/main/java/com/project1/training/revature/controller/UserController.java
@@ -1,8 +1,22 @@
 /**
- *
+ * Controller class is meant to contain HTTP methods like: Get, Post, Put, Delete
+ * We will use Postman app to send HTTP methods(Get,Post,Put,Delete) and those methods will be sent here to actually perform those operations
  */
 
 package com.project1.training.revature.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+    //http://localhost:8085/user/registerUser
+    @PostMapping("registerUser")
+    public String registerUser() {
+        return "User has been added";
+    }
 }
