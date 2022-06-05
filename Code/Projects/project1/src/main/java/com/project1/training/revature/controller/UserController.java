2fa4611e65a9f37712caa728bea9b8b58b7c47cc
zohairawan <zawan@luc.com>
2022-06-05 11:50:15 -0700

    Logging to file added to p1aws

--- Code/Projects/project1/src/main/java/com/project1/training/revature/controller/UserController.java
+++ Code/Projects/project1/src/main/java/com/project1/training/revature/controller/UserController.java
@@ -16,8 +16,6 @@ import com.project1.training.revature.model.User;
 import com.project1.training.revature.services.AuthorizationService;
 import com.project1.training.revature.services.UserService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
