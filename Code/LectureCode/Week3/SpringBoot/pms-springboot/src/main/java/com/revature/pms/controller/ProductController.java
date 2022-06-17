c7f0ec0d9a3e0275a312f5bbbef42255ee4ad077
zohairawan <zawan@luc.edu>
2022-06-17 00:11:02 -0700

    Added imports, no change though

--- Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/controller/ProductController.java
+++ Code/LectureCode/Week3/SpringBoot/pms-springboot/src/main/java/com/revature/pms/controller/ProductController.java
@@ -15,6 +15,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //This is at the class level so if you want to enter into this controller then you have to use localhost:8084/product
